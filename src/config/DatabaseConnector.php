bcef8113cad8c0a28a2e34c2f496ff38178b4fa3
Krzysiek <krzysztofkasowicz1995@gmail.com>
2021-02-01 15:27:50 +0100

    added configuration to connect with database

---
+++ src/config/DatabaseConnector.php
@@ -0,0 +1,33 @@
+<?php
+
+class DatabaseConnector
+{
+    private $connection = null;
+
+    /**
+     * DatabaseConnector constructor.
+     * @param null $connection
+     */
+    public function __construct()
+    {
+        $hostname = $_ENV['DB_HOST'];
+        $port = $_ENV['DB_PORT'];
+        $databaseName = $_ENV['DB_DATABASE'];
+        $username = $_ENV["DB_USERNAME"];
+        $password = $_ENV["DB_PASSWORD"];
+        try {
+            $this->connection = new PDO(
+                "mysql:host=$hostname;port=$port;charset=utf8mb4;dbname=$databaseName",
+                $username,
+                $password
+            );
+        }
+        catch (PDOException $e){
+            exit($e->getMessage());
+        }
+    }
+
+    public function getConnection(){
+        return $this->connection;
+    }
+}
